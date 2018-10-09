91f01f3637952e6937fb2d901c2fbe7eb5ed0be1
skaran226 <40988367+skaran226@users.noreply.github.com>
2018-10-09 17:38:20 +0530

    Add files via upload

---
+++ GTICLOUD/DB_Querys.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace GTICLOUD
+{
+    public class DB_Querys
+    {
+
+        internal static string LocalSqlConn() {
+
+            string query = "Data Source=LAPTOP-EMJVR3G8;Initial Catalog=gti_cloud;Integrated Security=True";
+            return query;
+        }
+
+        internal static string CreateAccountQuery() {
+
+            string query = "insert into registration (gti_user_name,email,category,password,created,updated) values (@username,@email,@category,@password,@created,@updated)";
+
+            return query;
+        }
+
+        internal static string CreateSite() {
+
+            string query = "insert into sites (sitename,siteid,postype,backofficeuserid,backofficepassword,regitered,updated) values (@sitename,@siteid,@postype,@backofficeuserid,@backofficepassword,@regitered,@updated)";
+
+            return query;
+        
+        }
+
+        internal static string GetRegisteredUsersQuery() {
+
+            string query = "select email,password,category from registration";
+
+            return query;
+        
+        }
+
+        internal static string GetSideNav() {
+
+            string query = "select *from sidenav";
+
+            return query;
+        
+        }
+
+        internal static string GetSites(string postype)
+        {
+            string query = "select sitekey,sitename,siteid,postype,regitered,updated from sites where postype='" + postype + "'";
+
+            return query;
+        }
+
+        internal static string SiteServices() {
+
+            return null;
+        
+        }
+
+        internal static string AddAccessControl()
+        {
+            string query = "insert into accessControl (sitekey,name,email,category,authentication_key,is_authenticate,created,updated,permission_level) values(@sitekey,@name,@email,@category,@authentication_key,@is_authenticate,@created,@updated,@permission_level)";
+
+            return query;
+        }
+
+        internal static string CheckAlready()
+        {
+            string query = "select email,sitekey from accessControl";
+
+            return query;
+        }
+    }
+}
---
+++ GTICLOUD/Macros.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace GTICLOUD
+{
+    public class Macros
+    {
+        public static readonly string SESSION_KEY = "global";
+        public static readonly string BACKOFFICE_KEY = "back123";
+        public static readonly string BACKOFFICE_VALUE = "back123";
+        public static readonly int iSUPER_ADMIN = 101;
+        public static readonly int iADMIN = 102;
+        public static readonly int iACCOUNT_MANAGER = 103;
+        public static readonly int iTECHNICIAN = 104;
+        public static readonly int iMANAGER = 105;
+        public static readonly int iEMPLOYEE = 106;
+        public static readonly int iCLIENT = 107;
+        public static readonly int iNOTACCESS = 0;
+        public static readonly string sADMIN = "admin";
+        public static readonly string sTECHNICIAN = "technician";
+        public static readonly string sACCOUNT_MANAGER = "account manager";
+        public static readonly string sCLIENT = "client";
+        public static readonly string sMANAGER = "manager";
+        public static readonly string sEMPLOYEE = "employee";
+    }
+}
